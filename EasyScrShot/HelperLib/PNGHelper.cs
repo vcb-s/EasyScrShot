5fa6e833382d586faa44b8a7cfb96b65bc090995
SaltyChiang <jxyzjchina@gmail.com>
2019-08-15 23:08:59 +0800

    Multithreads method added.

--- EasyScrShot/HelperLib/PNGHelper.cs
+++ EasyScrShot/HelperLib/PNGHelper.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Windows.Forms;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Drawing;
 using System.Drawing.Imaging;
 using PNGCompression;
@@ -10,22 +11,64 @@ namespace EasyScrShot.HelperLib
 {
     public static class PNGHelper
     {
-        private static string[] FileList { get; set; }
-
-        private static Thread thread;
-
         public static void MultiThreadPNGCompress(string[] fileList)
         {
-            int processorCount = Environment.ProcessorCount;
-
-            MultiThreadPNGCompress(fileList, processorCount);
+            MultiThreadPNGCompress(fileList, Environment.ProcessorCount);
         }
 
         public static void MultiThreadPNGCompress(string[] fileList, int processorCount)
         {
+            int i, j;
             int fileCount = fileList.Length;
+            Task[] tasks = new Task[fileCount];
+            Action<object> action = (object obj) =>
+                                    {
+                                        PNGCompress(obj);
+                                    };
 
+            j = 0;
+            for (i = 0; i < fileCount; i++)
+            {
+                tasks[i] = new Task(action, fileList[i]);
+                if (i < processorCount)
+                {
+                    tasks[i].Start();
+                }
+                else
+                {
+                    tasks[i - processorCount].Wait();
+                    tasks[i].Start();
+                }
+            }
+            for (i = 0; i < fileCount; i++)
+            {
+                tasks[i].Wait();
+            }
 
+            /*
+            WaitCallback wait = fileName =>
+            {
+                PNGCompress(fileName);
+            };
+            ThreadPool.SetMaxThreads(processorCount, processorCount * 10);
+            for (i = 0; i < fileCount; i++)
+            {
+                ThreadPool.QueueUserWorkItem(wait, fileList[i]);
+            }
+            ThreadPool.GetAvailableThreads(out i, out j);
+            while (i < processorCount)
+            {
+                ThreadPool.GetAvailableThreads(out i, out j);
+                // Thread.Sleep(1000);
+            }
+            */
+
+            RemoveOptiPng();
+        }
+
+        private static void PNGCompress(object fileNameObject)
+        {
+            PNGCompress(fileNameObject.ToString());
         }
 
         private static void PNGCompress(string fileName)
@@ -34,7 +77,6 @@ namespace EasyScrShot.HelperLib
             LosslessInputSettings inputSettings = new LosslessInputSettings();
             // inputSettings.OptimizationLevel = "3";
             compressor.CompressImageLossLess(fileName, Utility.CurrentDir + $"temp." + fileName, inputSettings);
-            RemoveOptiPng();
         }
 
         private static void RemoveOptiPng()
